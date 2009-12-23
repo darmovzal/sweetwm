609c5e80c2b469f6c9fc5891768d194e66d3218b
Tomas Darmovzal <tomas.darmovzal@gmail.com>
2009-12-23 18:38:23 +0100

    Removed "xevent" table - events moved to parent.

--- sweetwm.lua
+++ sweetwm.lua
@@ -1,70 +1,35 @@
 sweetwm.events = {
-	xevent = {
-		map_request = function(id)
-			sweetwm.process_window(id, print)
-			local window = sweetwm.window
-			sweetwm.windows[id] = window
-			sweetwm.window = nil
-			local x, y, w, h = sweetwm.get_window_size(window)
-			sweetwm.move_window(id, x, y)
-			sweetwm.resize_window(id, w, h)
-			sweetwm.set_border_width(id, 5)
-			sweetwm.set_border_color(id, "rgb:ff/00/00")
-			sweetwm.map_window(id)
-		end,
-		configure_request = function(id, x, y, w, h)
-			sweetwm.move_window(id, x, y)
-			sweetwm.resize_window(id, w, h)
-		end,
-		enter_notify = function(id)
-			print("Enter", id)
-		end,
-		leave_notify = function(id)
-		end,
-		create_notify = function()
-		end,
-		destroy_notify = function()
-		end,
-		map_notify = function()
-		end,
-		unmap_notify = function()
-		end,
-		configure_notify = function()
-		end,
-		key_press = function(key)
-		end,
-		key_release = function(key)
-		end,
-	},
---[[
-	window = {
-		begin = function(w)
-			sweetwm.window = {
-				id = w,
-				attributes = {},
-				properties = {},
-				wmhints = {},
-				sizehints = {}
-			}
-		end,
-		attribute = function(name, value)
-			sweetwm.window.attributes[name] = value
-		end,
-		property = function(name, value)
-			sweetwm.window.properties[name] = value
-		end,
-		wmhint = function(name, value)
-			sweetwm.window.wmhints[name] = value
-		end,
-		sizehint = function(name, value)
-			sweetwm.window.sizehints[name] = value
-		end,
-		["end"] = function() end
-	}
-]]
+	map_request = function(id)
+		local x, y, w, h = sweetwm.get_window_size(id)
+		sweetwm.move_window(id, x, y)
+		sweetwm.resize_window(id, w, h)
+		sweetwm.set_border_width(id, 5)
+		sweetwm.set_border_color(id, "rgb:ff/00/00")
+		sweetwm.map_window(id)
+	end,
+	configure_request = function(id, x, y, w, h)
+		sweetwm.move_window(id, x, y)
+		sweetwm.resize_window(id, w, h)
+	end,
+	enter_notify = function(id)
+	end,
+	leave_notify = function(id)
+	end,
+	create_notify = function()
+	end,
+	destroy_notify = function()
+	end,
+	map_notify = function()
+	end,
+	unmap_notify = function()
+	end,
+	configure_notify = function()
+	end,
+	key_press = function(key)
+	end,
+	key_release = function(key)
+	end,
 }
-
-sweetwm.windows = {}
 
 function sweetwm.error(...)
 	print("Error:", ...)
@@ -91,14 +56,52 @@ function sweetwm.event(name, ...)
 	end
 end
 
-function sweetwm.get_window_size(window)
-	return window.attributes.x + 50, window.attributes.y + 50, window.attributes.width, window.attributes.height
+function sweetwm.get_window(id)
+	local window = {
+		attributes = {},
+		properties = {},
+		wmhints = {},
+		sizehints = {}
+	}
+	local callbacks = {
+		attribute = function(name, value)
+			window.attributes[name] = value
+		end,
+		property = function(name, value)
+			window.properties[name] = value
+		end,
+		wmhint = function(name, value)
+			window.wmhints[name] = value
+		end,
+		sizehint = function(name, value)
+			window.sizehints[name] = value
+		end,
+		}
+	sweetwm.process_window(id, function(event, ...)
+		callbacks[event](...)
+	end)
+	return window
 end
 
-for n, v in pairs(sweetwm) do
-	print(n, v)
+function sweetwm.get_window_size(id)
+	local window = sweetwm.get_window(id)
+	local root = sweetwm.get_window(window.attributes.root)
+	local x = window.attributes.x
+	local y = window.attributes.y
+	local w = window.attributes.width
+	local h = window.attributes.height
+	local rw = root.attributes.width
+	local rh = root.attributes.height
+	if x == 0 and y == 0 then
+		x = (rw - w) / 2
+		y = (rh - h) / 2
+	end
+	return x, y, w, h
 end
+
 print("Screens:")
-print(sweetwm.get_screen_size(0))
+for screen = 0, sweetwm.get_screen_count() - 1 do
+	print(screen .. ":", sweetwm.get_screen_size(screen))
+end
 
 
